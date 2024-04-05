65bac0a81fdf2f919d1bfb5456daae4ae5a1850a
qmr1cob <saranishkumar11@gmail.com>
2024-04-05 21:24:03 +0530

    fix

--- main.py
+++ main.py
@@ -4,8 +4,13 @@ from fastapi.responses import JSONResponse
 from pymongo import MongoClient
 import uvicorn
 import asyncio
-import socks
 
+# os.environ["http_proxy"] = "http://10.143.16.65:8080"
+# os.environ["https_proxy"] = "http://10.143.16.65:8080"
+# os.environ["HTTP_PROXY"] = "http://10.143.16.65:8080"
+# os.environ["HTTPS_PROXY"] ="http://10.143.16.65:8080"
+# os.environ["NO_PROXY"] = ".de.bosch.com,.bosch.com,.cluster.local,.svc,10.140.180.0/23,10.140.214.0/24,10.140.249.0/24,10.140.250.30,10.140.254.0/24,10.40.0.0/24,127.0.0.1,169.254.169.254,192.168.0.0/17,192.168.128.0/17,api-int.de3pro.osh.ipz001.internal.bosch.cloud,etcd-0.de3pro.osh.ipz001.internal.bosch.cloud,etcd-1.de3pro.osh.ipz001.internal.bosch.cloud,etcd-2.de3pro.osh.ipz001.internal.bosch.cloud,internal.bosch.cloud,localhost,osh.ipz001.internal.bosch.cloud"
+# os.environ["PROXY_URL"] = "http://rb-proxy-sl.bosch.com:8080"
 # Initialize FastAPI app
 app = FastAPI()
 
@@ -14,15 +19,10 @@ MONGO_URI = os.getenv("MONGO_URI")
 MONGO_DB = os.getenv("MONGO_DB")
 MONGO_COLLECTION = os.getenv("MONGO_COLLECTION")
 
-# SOCKS5 Proxy configuration
-PROXY_HOST = os.getenv("SOCKS5_PROXY_HOST")
-PROXY_PORT = os.getenv("SOCKS5_PROXY_PORT")
-
-# Connect to MongoDB through SOCKS5 proxy
-proxy = socks.socksocket()
-proxy.set_proxy(socks.SOCKS5, PROXY_HOST, int(PROXY_PORT))
-client = MongoClient(MONGO_URI, socket=proxy)
+# Connect to MongoDB
+client = MongoClient(MONGO_URI)
 db = client[MONGO_DB]
+print(db)
 collection = db[MONGO_COLLECTION]
 
 # Routes
